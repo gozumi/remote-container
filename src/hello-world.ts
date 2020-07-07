897d8982bbdab1994e12d7489cccf66a3602814b
Ian Anderson <ian@gozumi.com>
2020-07-07 23:49:11 +0100

    chore: Connect to mongo container from the app container.

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,2 +1,19 @@
-//noddy hello world
-console.log('hello world at', new Date().toISOString());
+import { MongoClient } from 'mongodb'
+
+
+console.log('>>>> setting connection url')
+
+const { DB_HOSTNAME } = process.env
+const url = `mongodb://${DB_HOSTNAME}:27017`
+
+
+MongoClient.connect(url, { useUnifiedTopology: true  })
+  .then((client) => {
+    const db = client.db('sr')
+    const collection = db.collection('demo')
+    console.log('>>>>> inserting into mongo')
+    collection.insertOne({ hello: 'world' })
+  })
+  .catch((error) => console.log('>>>>> error', error))
+
+
